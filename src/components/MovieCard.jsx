363e6016abbcfe385bae89d912c427d00aa1b856
pasqualedimaro <pakydm15@gmail.com>
2025-08-04 12:56:43 +0200

    fix color

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -18,7 +18,7 @@ const MovieCard = ({ data }) => {
                 <p className="card-text">{abstract}</p>
                 <StarRating media_voti={media_voti} />
                 {/* <p className="card-text"><StarComponent/></p> */}
-                <Link to={`/movie/${id}`} className="btn btn-primary">Dettaglio</Link>
+                <Link to={`/movie/${id}`} className="btn">Dettaglio</Link>
             </div>
         </div>
     )
